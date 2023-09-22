fe06f7c4d732c6793ff49a35e1cef3482721508a
gtz <gtzafw@gmail.com>
2023-09-22 10:38:54 -0700

    add CORS

--- server/__init__.py
+++ server/__init__.py
@@ -4,6 +4,7 @@ import sys
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_cors import CORS
 
 # SQLite URI compatible
 WIN = sys.platform.startswith('win')
@@ -16,6 +17,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev')
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path), os.getenv('DATABASE_FILE', 'data.db'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
